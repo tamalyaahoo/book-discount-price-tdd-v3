e5ba372c30d070db0a3054016e4a66b7e4823835
TAMAL NAYEK <tamal.nayek@yahoo.com>
2025-12-07 09:18:52 +0530

    feat: initial Spring Boot project setup. Create new spring boot 4.0 and Java 21 project from scratch. Closes #1

---
+++ book-discount-price-backend/src/main/java/com/bnpp/kata/book/price/BookDiscountPriceApplication.java
@@ -0,0 +1,13 @@
+package com.bnpp.kata.book.price;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDiscountPriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookDiscountPriceApplication.class, args);
+	}
+
+}
---
+++ book-discount-price-backend/src/test/java/com/bnpp/kata/book/price/BookDiscountPriceApplicationTest.java
@@ -0,0 +1,14 @@
+package com.bnpp.kata.book.price;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class BookDiscountPriceApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
